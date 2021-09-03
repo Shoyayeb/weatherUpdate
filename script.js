1cde669ccf5efc706905e4c76fe1a995a3cb8311
Shafin <sa.fine@outlook.com>
2021-09-03 23:40:25 +0600

    result ui updated

--- script.js
+++ script.js
@@ -10,15 +10,36 @@ function fetchCurrentWeather() {
 const showCurrentWeather = weather => {
     console.log(weather);
     const iconCode = weather.weather[0].icon;
-    const iconUrl = ` http://openweathermap.org/img/wn/${iconCode}@4x.png`
-    const conditionImage = document.getElementById("currentWeatherImg")
+    const iconUrl = ` http://openweathermap.org/img/wn/${iconCode}@4x.png`;
+    const conditionImage = document.getElementById("currentWeatherImg");
+    const logoImg = document.getElementById("logoImg");
+    logoImg.src = iconUrl
     conditionImage.src = iconUrl;
+
+
+
+
+    var link = document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = iconUrl;
+    // document.getElementsByTagName('head')[0].removeChild("link")
+    document.getElementsByTagName('head')[0].appendChild(link);
+
+
+
+
+
+
+
+
     // text
     const conditionText = document.getElementById("conditionText");
     const locationGot = weather.name;
     const descriptionGot = weather.weather[0].description;
     const tempGot = weather.main.temp;
     conditionText.innerText = `Its ${descriptionGot} in ${locationGot} and the temparature is ${tempGot}°`
+
 
 }
 
